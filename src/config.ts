683bbae9329427f236222552ffd9709967da5477
AhogeK <ahogek@gmail.com>
2023-01-17 20:48:54 +0800

    fix(meta): udpate website info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'AhogeK的个人博客';
+export const SITE_DESCRIPTION = '欢迎来到我的网站!👏';
+export const HOMEPAGE_URL = 'http://ahogek.com';
