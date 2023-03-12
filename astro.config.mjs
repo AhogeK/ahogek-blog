0647b0147583bebe39c6d5cc912d8dd8bf2fea89
AhogeK <ahogek@gmail.com>
2023-03-12 18:17:53 +0800

    docs: astro 配置注解

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+// 用于创建一个 Astro 配置对象，可以用来配置 Astro 应用程序的各种选项和插件。
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
@@ -6,6 +7,14 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  // 网站根地址
   site: 'http://blob.ahogek.com',
+  /*
+   * 整合插件
+   * sitemap：生成站点地图
+   * react：支持 React 组件
+   * image：支持图片
+   * tailwind：支持 Tailwind CSS
+   */
   integrations: [sitemap(), react(), image(), tailwind()]
 });
