6463a2b36ad321ecfb5debbe9fe54eca32a84106
AhogeK <ahogek@gmail.com>
2023-03-12 18:58:19 +0800

    build: 依赖的更新与更改

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'AhogeK的个人博客';
+export const SITE_TITLE = 'AhogeK';
 export const SITE_DESCRIPTION = '欢迎来到我的网站!👏';
-export const HOMEPAGE_URL = 'http://blob.ahogek.com';
+export const HOMEPAGE_URL = 'http://blog.ahogek.com';
