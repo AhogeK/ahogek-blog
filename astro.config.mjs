af7dcfb27a51793e97c2492ccf7e6ed62954ccf1
AhogeK <ahogek@gmail.com>
2023-04-26 18:43:41 +0800

    fix(import): error io5 import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@
  * @author AhogeK ahogek@gmail.com
  * @date 2022-01-16
  */
-
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
@@ -11,12 +10,12 @@ import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-  site: 'http://blob.ahogek.com',           // 网站根地址
-  integrations: [                           // 整合插件
-    sitemap(),                              // 生成站点地图
-    react(),                                // 支持 React 组件
-    image(),                                // 支持图片
-    tailwind()                              // 支持 Tailwind CSS
+  site: 'https://blob.ahogek.com', // 网站根地址
+  integrations: [
+    // 整合插件
+    sitemap(), // 生成站点地图
+    react(), // 支持 React 组件
+    image(), // 支持图片
+    tailwind() // 支持 Tailwind CSS
   ]
-});
-
+})
