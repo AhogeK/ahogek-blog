5f1e6f6985b21c7f9511876f36c3f0764d1f383a
AhogeK <ahogek@gmail.com>
2023-12-01 16:42:36 +0800

    feat: 轮播视频

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -9,31 +9,45 @@ const Video: React.FC = () => {
   const [isMuted, setIsMuted] = useState(true)
   const [showVolume, setShowVolume] = useState(false)
   const [nodeJSTimeout, setNodeJSTimeout] = useState<NodeJS.Timeout>()
+  const [videoList, setVideoList] = useState<string[]>([
+    'https://ahogek.com/uploads/video/20231201/cherry_blossom_tree_artistic_confession/cherry_blossom_tree_artistic_confession.m3u8',
+    'https://ahogek.com/uploads/video/20231029/one-more-time-one-more-chance/one-more-time-one-more-chance.m3u8'
+  ])
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0)
 
   const video = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
     if (video.current) {
-      video.current.volume = 0.7
+      if (currentVideoIndex === 0) {
+       video.current.volume = 0.3
+      } else {
+        video.current.volume = 0.7
+      }
       if (Hls.isSupported()) {
         let hls = new Hls()
-        hls.loadSource(
-          'https://ahogek.com/uploads/video/20231029/one-more-time-one-more-chance/one-more-time-one-more-chance.m3u8'
-        )
+        hls.loadSource(videoList[currentVideoIndex])
         hls.attachMedia(video.current)
         hls.on(Hls.Events.MANIFEST_PARSED, function() {
           video.current?.play()
         })
+        video.current.onended = function() {
+          console.log('end')
+          setCurrentVideoIndex((currentVideoIndex + 1) % videoList.length)
+        }
       } else if (video.current.canPlayType('application/vnd.apple.mpegurl')) {
-        video.current.src =
-          'https://ahogek.com/uploads/video/20231029/one-more-time-one-more-chance/one-more-time-one-more-chance.m3u8'
+        video.current.src = videoList[currentVideoIndex]
         video.current.addEventListener('canplay', function() {
           video.current?.play()
         })
+        video.current.onended = function() {
+          console.log('end')
+          setCurrentVideoIndex((currentVideoIndex + 1) % videoList.length)
+        }
       }
     }
     setShowVolume(true)
-  }, [])
+  }, [currentVideoIndex])
 
   useEffect(() => {
     if (hidingTime) {
@@ -64,47 +78,43 @@ const Video: React.FC = () => {
   }, [isMuted, showVolume])
 
   return (
-    <>
-      <div className='absolute w-full h-full overflow-hidden'
-           onMouseEnter={() => {
-             if (!isMobile) {
+    <div className='absolute w-full h-full overflow-hidden'
+         onMouseEnter={() => {
+           if (!isMobile) {
+             setShowVolume(true)
+             clearTimeout(nodeJSTimeout)
+             setHidingTime(null)
+           }
+         }}
+         onMouseLeave={() => {
+           if (!isMobile)
+             setHidingTime(1000)
+         }}
+         onClick={() => {
+           if (isMobile) {
+             if (!showVolume) {
                setShowVolume(true)
-               clearTimeout(nodeJSTimeout)
-               setHidingTime(null)
              }
-           }}
-           onMouseLeave={() => {
-             if (!isMobile)
-               setHidingTime(1000)
-           }}
-           onClick={() => {
-             if (isMobile) {
-               if (!showVolume) {
-                 setShowVolume(true)
-               }
-               setHidingTime(null)
-               setHidingTime(3000)
-             }
-           }}
-      >
-        <div className='uppercase text-sm mb-4'>
-          <video
-            id='video'
-            className='absolute h-auto left-1/2 top-1/2 min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2 opacity-30'
-            autoPlay
-            loop
-            muted
-            playsInline
-            preload='auto'
-            ref={video}
-
-          ></video>
-          <div onClick={handleVolume} className='absolute bottom-2 right-2'>
-            {handleVolumeIcon}
-          </div>
+             setHidingTime(null)
+             setHidingTime(3000)
+           }
+         }}
+    >
+      <div className='uppercase text-sm mb-4'>
+        <video
+          id='video'
+          className='absolute h-auto left-1/2 top-1/2 min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2 opacity-30'
+          autoPlay
+          muted
+          playsInline
+          preload='auto'
+          ref={video}
+        ></video>
+        <div onClick={handleVolume} className='absolute bottom-2 right-2'>
+          {handleVolumeIcon}
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
